5a1ad59a22744d940b85ec49af86f42b94cb7d79
Adans_lnx <jirschik@hotmail.com>
2026-01-14 00:39:13 -0300

    HP com hora

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ namespace MEUSITE
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            // Nenhum serviço necessário para app mínima
+            // Configuração de serviços
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -20,23 +20,14 @@ namespace MEUSITE
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async context =>
-            {
-                context.Response.ContentType = "text/html; charset=utf-8";
+            app.UseRouting();
 
-                await context.Response.WriteAsync(@"
-                    <!DOCTYPE html>
-                    <html lang='pt-BR'>
-                    <head>
-                        <meta charset='utf-8'>
-                        <title>Meu Site</title>
-                    </head>
-                    <body style='background-color: #2c3e50; color: white; font-family: Arial, Helvetica, sans-serif;'>
-                        <h1>Bem-vindo ao Site Projeto 2!</h1>
-                        <p>Novo deploy realizado com sucesso.</p>
-                    </body>
-                    </html>
-                ");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Bem-vindo ao Site Projeto 2! - Novo deploy realizado em " + DateTime.Now);
+                });
             });
         }
     }
